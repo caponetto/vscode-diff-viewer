12523789bb5a3d82a2fd4821354c7207ed1a1099
Jacek Kopecky <jacek@jacek.cz>
2022-08-04 11:56:27 +0100

    Add a footer like "viewed 2/8"

--- static/app.js
+++ static/app.js
@@ -9,12 +9,14 @@
     diff2htmlUi.draw();
 
     registerViewToggleHandlers(targetElement);
+    updateFooter();
   });
 
   function registerViewToggleHandlers(root) {
     const viewedToggles = root.querySelectorAll(".d2h-file-collapse-input");
     for (const el of viewedToggles) {
       el.addEventListener("change", scrollHeaderIntoView);
+      el.addEventListener("change", updateFooter);
     }
   }
 
@@ -22,6 +24,15 @@
     const headerEl = e.target.closest(".d2h-file-header");
     if (headerEl) {
       headerEl.scrollIntoView({ block: "nearest" });
+    }
+  }
+
+  function updateFooter() {
+    const footer = document.querySelector("footer");
+    if (footer) {
+      const allCount = document.querySelectorAll(".d2h-file-collapse-input").length;
+      const viewedCount = document.querySelectorAll(".d2h-file-collapse-input:checked").length;
+      footer.textContent = `viewed ${viewedCount}/${allCount}`;
     }
   }
 })();
