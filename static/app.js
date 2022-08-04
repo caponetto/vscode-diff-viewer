72f9e1ac05703348c41e1469ca6f6fb256f1d1b1
Jacek Kopecky <jacek@jacek.cz>
2022-08-04 09:31:28 +0100

    Make file headers sticky
    `scrollIntoView` is necessary in case we are far through a long file;
    as we click "viewed" the file will collapse and other files or chunks
    could end up above the screen.

--- static/app.js
+++ static/app.js
@@ -7,5 +7,21 @@
 
     const diff2htmlUi = new Diff2HtmlUI(targetElement, diffFiles, config);
     diff2htmlUi.draw();
+
+    registerViewToggleHandlers(targetElement);
   });
+
+  function registerViewToggleHandlers(root) {
+    const viewedToggles = root.querySelectorAll(".d2h-file-collapse-input");
+    for (const el of viewedToggles) {
+      el.addEventListener("change", scrollHeaderIntoView);
+    }
+  }
+
+  function scrollHeaderIntoView(e) {
+    const headerEl = e.target.closest(".d2h-file-header");
+    if (headerEl) {
+      headerEl.scrollIntoView({ block: "nearest" });
+    }
+  }
 })();
