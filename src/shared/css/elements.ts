44296d3db9c8dbbf03a575a9cd8130bc2b367c77
Jacek Kopecky <jacek@jacek.cz>
2023-01-15 10:50:40 +0000

    Show empty diffs

--- src/shared/css/elements.ts
+++ src/shared/css/elements.ts
@@ -1,6 +1,7 @@
 export enum SkeletonElementIds {
   DiffContainer = "diff-container",
   LoadingContainer = "loading-container",
+  EmptyMessageContainer = "empty-message-container",
   ViewedResetButton = "viewed-reset-button",
   ViewedIndicator = "viewed-indicator",
 }
