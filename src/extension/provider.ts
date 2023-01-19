0eab2168ceb744f8ff833994aa0dfda3c4fde0be
Jacek Kopecký <jacek@jacek.cz>
2023-01-19 20:05:34 +0000

    Add a side-by-side toggle (#67)
    * Add a side-by-side toggle
    
    * Fix to use the global setting
    
    * Use better icons
    
    * :art:
    
    * Shorten minimum loading delay

--- src/extension/provider.ts
+++ src/extension/provider.ts
@@ -3,7 +3,7 @@ import { basename } from "path";
 import * as vscode from "vscode";
 import { SkeletonElementIds } from "../shared/css/elements";
 import { MessageToExtensionHandler, MessageToWebview } from "../shared/message";
-import { APP_CONFIG_SECTION, extractConfig } from "./configuration";
+import { APP_CONFIG_SECTION, extractConfig, setOutputFormatConfig } from "./configuration";
 import { MessageToExtensionHandlerImpl } from "./message/handler";
 import { buildSkeleton } from "./skeleton";
 import { ViewedStateStore } from "./viewed-state";
@@ -24,14 +24,18 @@ export class DiffViewerProvider implements vscode.CustomTextEditorProvider {
 
   public constructor(private readonly args: DiffViewerProviderArgs) {}
 
-  public static register(args: DiffViewerProviderArgs): vscode.Disposable {
-    return vscode.window.registerCustomEditorProvider(DiffViewerProvider.VIEW_TYPE, new DiffViewerProvider(args), {
-      webviewOptions: {
-        retainContextWhenHidden: true,
-        enableFindWidget: true,
-      },
-      supportsMultipleEditorsPerDocument: false,
-    });
+  public static registerContributions(args: DiffViewerProviderArgs): vscode.Disposable[] {
+    return [
+      vscode.window.registerCustomEditorProvider(DiffViewerProvider.VIEW_TYPE, new DiffViewerProvider(args), {
+        webviewOptions: {
+          retainContextWhenHidden: true,
+          enableFindWidget: true,
+        },
+        supportsMultipleEditorsPerDocument: false,
+      }),
+      vscode.commands.registerCommand("diffviewer.showLineByLine", () => setOutputFormatConfig("line-by-line")),
+      vscode.commands.registerCommand("diffviewer.showSideBySide", () => setOutputFormatConfig("side-by-side")),
+    ];
   }
 
   public async resolveCustomTextEditor(
@@ -138,7 +142,7 @@ export class DiffViewerProvider implements vscode.CustomTextEditorProvider {
           },
         },
       });
-    }, 500);
+    }, 100);
   }
 
   private postMessageToWebviewWrapper(args: { webview: vscode.Webview; message: MessageToWebview }): void {
